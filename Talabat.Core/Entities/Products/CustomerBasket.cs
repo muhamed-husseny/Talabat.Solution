e29aca7c035c9b73cd54fd6709b48abe67a11ae3
Muhamed Husseny <muhamedhusseny288@gmail.com>
2025-12-08 14:10:49 +0200

    BasketModuleEntities

---
+++ Talabat.Core/Entities/Products/CustomerBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Products
+{
+    public class CustomerBasket
+    {
+        public int Id { get; set; }
+
+        public List<BasketItem> items { get; set; }
+    }
+}
