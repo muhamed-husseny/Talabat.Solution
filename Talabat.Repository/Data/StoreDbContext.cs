7fc844e8055643e70762eb20a24c7efeef15431b
Muhamed Husseny <muhamedhusseny288@gmail.com>
2025-12-15 18:29:37 +0200

    Migration and update Database

--- Talabat.Repository/Data/StoreDbContext.cs
+++ Talabat.Repository/Data/StoreDbContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Talabat.Core.Entities.Order_Aggregate;
 using Talabat.Core.Entities.Products;
 
 namespace Talabat.Repository.Data
@@ -21,9 +22,12 @@ namespace Talabat.Repository.Data
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
         public DbSet<Product> Products { get; set; }
-
         public DbSet<ProductBrand> ProductBrands { get; set; }
-
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+       
+
     }
 }
